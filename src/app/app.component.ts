6441e43acbf1371a25856cc6eea27cd8dc6d79be
Damian Bajno <bajno.damian96@gmail.com>
2022-02-09 21:57:11 +0100

    Exercise with Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular13';
+  username: string = "Damian";
+
+  clearUsername() {
+    this.username = '';
+  }
 }
