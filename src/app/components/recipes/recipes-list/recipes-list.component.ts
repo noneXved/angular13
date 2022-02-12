1130799ab16d63d8f09e58133df5a12658c0826f
Damian Bajno <bajno.damian96@gmail.com>
2022-02-12 21:26:37 +0100

    Added style to components

--- src/app/components/recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('a', 'dafsdfs', 'https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?quality=45&resize=768,574'),
+    new Recipe('TEST', 'dafsdfs', 'https://images.immediate.co.uk/production/volatile/sites/2/2019/04/Dum-Aloo-e163632.jpg?quality=45&resize=768,574')
+  ];
 
   constructor() { }
 
