3893163a1ce30ec437b757606ec51f3823cf8600
Damian Bajno <bajno.damian96@gmail.com>
2022-02-13 21:23:03 +0100

    Basic preparation for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NewAccountComponent} from "./new-account/new-account.component";
+import {AccountComponent} from "./account/account.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewAccountComponent,
+    AccountComponent
   ],
     imports: [
         BrowserModule,
