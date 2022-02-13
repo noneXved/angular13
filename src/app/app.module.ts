a9d87b712d7ab562582d0449318a57f1250cdd41
Damian Bajno <bajno.damian96@gmail.com>
2022-02-13 17:52:02 +0100

    [Directive attribute] Creating own directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BasicHighlightDirective } from './directives/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
     imports: [
         BrowserModule,
