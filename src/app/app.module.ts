cc0592e0bc28375a227f9499f8b060d30b38e124
Damian Bajno <bajno.damian96@gmail.com>
2022-02-13 17:44:19 +0100

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { RecipesComponent } from './components/recipes/recipes.component';
-import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './components/recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './components/recipes/recipes-item/recipes-item.component';
-import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    RecipesItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
+    AppComponent
   ],
     imports: [
         BrowserModule,
