37e63aee433387607f38612d1567874286a91a89
Damian Bajno <bajno.damian96@gmail.com>
2022-02-13 23:02:02 +0100

    Basic structure for practice routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from "./home/home.component";
+import {UsersComponent} from "./users/users.component";
+import {ServersComponent} from "./servers/servers.component";
+import {EditServerComponent} from "./servers/edit-server/edit-server.component";
+import {ServerComponent} from "./servers/server/server.component";
+import {UserComponent} from "./users/user/user.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UsersComponent,
+    UserComponent
   ],
     imports: [
         BrowserModule,
