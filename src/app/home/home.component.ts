57988a33ec62b2fa18d2bc95027ef96df17f9d7c
Damian Bajno <bajno.damian96@gmail.com>
2022-02-14 11:06:07 +0100

    [Observable Operators] filter, map, pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Observable, Subscription} from "rxjs";
+import {filter, map, Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 2) {
+        if (count === 5) {
           observer.complete();
         }
         if (count > 3) {
@@ -25,7 +25,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map( (data: number) => {
+      return (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
