2f12330cb7da5a9c67ef5c157522d49d5ccb75ab
Damian Bajno <bajno.damian96@gmail.com>
2022-02-14 18:24:02 +0100

    [Form] Set Default Values for form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,27 @@ export class AppComponent {
   @ViewChild('form') signupForm: NgForm;
   defaultQuestion: string = 'teacher';
   answer: string = '';
-
+  genders = [ 'male', 'female']
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // Set default values for all cells (override)
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    })
+
+    // Set default values for empty cells
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   onSubmit() {
