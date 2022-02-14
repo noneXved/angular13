2e7a03ae5d993b7ce32044f0cd73641e7738de1a
Damian Bajno <bajno.damian96@gmail.com>
2022-02-14 18:35:56 +0100

    [Form] Display data and reset form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   defaultQuestion: string = 'teacher';
   answer: string = '';
   genders = [ 'male', 'female']
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted: boolean = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
 
@@ -34,6 +43,13 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.signupForm)
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset()
   }
 }
