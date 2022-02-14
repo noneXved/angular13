620af64c324b6fb858bafbe20bc71b4615c615b5
Damian Bajno <bajno.damian96@gmail.com>
2022-02-14 17:35:39 +0100

    [Form Validation] Three types binding 1. no binding 2. one way binding 3. two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {NgForm} from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild('form') signupForm: NgForm;
+  defaultQuestion: string = 'teacher';
+  answer: string = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
