a09112f2ff725030737401469846d57363de8602
Damian Bajno <bajno.damian96@gmail.com>
2022-02-14 10:01:13 +0100

    Basic template project Observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {UserComponent} from "./user/user.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    UserComponent
 ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        BrowserAnimationsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
