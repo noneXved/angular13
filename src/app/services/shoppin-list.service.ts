634852b6c28c88cf0a28fca34d269ce2164dc2e5
Damian Bajno <bajno.damian96@gmail.com>
2022-02-15 22:33:49 +0100

    Added http client to shopping service

--- src/app/services/shoppin-list.service.ts
+++ src/app/services/shoppin-list.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppinListService {
+
+  constructor(private http: HttpClient) {
+  }
+
 }
