62d5941a7e276c7b2fb1fcb8a57c1362c28fab4a
noneXved <bajno.damian96@gmail.com>
2022-02-20 17:05:15 +0100

    [Pipes] Customizing own pipe

--- src/app/shortenpipe.pipe.ts
+++ src/app/shortenpipe.pipe.ts
@@ -1,11 +1,14 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'shortenpipe'
 })
 export class ShortenpipePipe implements PipeTransform {
-  transform(value: any): any {
-    return value.substr(0, 10) + ' ...';
+  transform(value: any, limit: number) {
+    if (value.length > limit) {
+      return value.substr(0, limit) + ' ...';
+    }
+    return value;
   }
 
 }
