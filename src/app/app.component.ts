49a4085146d80f725d7f34d7137e933acba1f65b
noneXved <bajno.damian96@gmail.com>
2022-02-20 16:28:19 +0100

    [Dynamic Components] Using @Output & EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  error: string = 'This password is not correct!';
+  isVisible: boolean = false;
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isVisible = true;
+    }, 1000)
+  }
+
+  onHandleError() {
+    this.isVisible = false;
+  }
+
+
+
+  onChangePassword(): void {
+    {}
+  }
 }
